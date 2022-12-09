83a85b409682c61f1950d4f29280962ff1de50a8
objectbased <67526309+objectbased@users.noreply.github.com>
2022-12-09 01:25:00 -0500

    c
    c

---
+++ bin/config.py
@@ -0,0 +1,100 @@
+feeds = { "sources": 
+    [
+        {
+            "name": "CoinBlocker",
+            "url": "https://zerodot1.gitlab.io/CoinBlockerLists/list.txt",
+            "type": ["domain"],
+            "regex": "N/A"
+        },
+        {
+            "name": "blocklist.de-all",
+            "url": "https://lists.blocklist.de/lists/all.txt",
+            "type": ["ip"],
+            "regex": "N/A"
+        },
+        {
+            "name": "OpenPhish",
+            "url": "https://openphish.com/feed.txt",
+            "type": ["url"],
+            "regex": "N/A"
+        },
+        {
+            "name": "dan.me TOR Exit Nodes",
+            "url": "https://www.dan.me.uk/torlist/",
+            "type": ["ip"],
+            "regex": "N/A"
+        },
+        {
+            "name": "Greensnow",
+            "url": "http://blocklist.greensnow.co/greensnow.txt",
+            "type": ["ip"],
+            "regex": "N/A"
+        },
+        {
+            "name": "Botvrij.eu - ips",
+            "url": "http://www.botvrij.eu/data/ioclist.ip-dst.raw",
+            "type": ["ip"],
+             "regex": "N/A"
+        },
+        {
+            "name": "URLHause abuse.ch",
+            "url": "https://urlhaus.abuse.ch/downloads/text/",
+            "type": ["url"],
+            "regex": "N/A"
+        },
+        {
+            "name": "Brute force Blockers",
+            "url": "http://danger.rulez.sk/projects/bruteforceblocker/blist.php",
+            "type": ["ip"],
+            "regex": "(\d{0,3}\.\d{0,3}\.\d{0,3}\.\d{0,3})"
+        },
+        {
+            "name": "Alienvault",
+            "url": "http://reputation.alienvault.com/reputation.data",
+            "type": ["ip"],
+            "regex": "^(.*?)\#"
+        },
+        {
+            "name": "CI Bad Guys",
+            "url": "http://cinsscore.com/list/ci-badguys.txt",
+            "type": ["ip"],
+            "regex": "N/A"
+        },
+        {
+            "name": "Emerging Threats",
+            "url": "https://rules.emergingthreats.net/blockrules/compromised-ips.txt",
+            "type": ["ip"],
+            "regex": "N/A"
+        },
+        {
+            "name": "Darklist",
+            "url": "https://www.darklist.de/raw.php",
+            "type": ["ip"],
+            "regex": "N/A"
+        },
+        {
+            "name": "SSH dictonary Attacks",
+            "url": "https://charles.the-haleys.org/ssh_dico_attack_hdeny_format.php/hostsdeny.txt",
+            "type": ["ip"],
+            "regex": "(\d{0,3}\.\d{0,3}\.\d{0,3}\.\d{0,3})"
+        },
+        {
+            "name": "SSL BL - abuse.ch",
+            "url": "https://sslbl.abuse.ch/blacklist/sslipblacklist.csv",
+            "type": ["ip"],
+            "regex": "(\d{0,3}\.\d{0,3}\.\d{0,3}\.\d{0,3})"
+        },
+        {
+            "name": "Talos IP Blacklist",
+            "url": "http://www.talosintelligence.com/documents/ip-blacklist",
+            "type": ["ip"],
+            "regex": "N/A"
+        },
+        {
+            "name": "SSC-Threat-Intel-IoCs",
+            "url": "https://github.com/securityscorecard/SSC-Threat-Intel-IoCs/blob/master/KillNet-DDoS-Blocklist/proxylist.txt",
+            "type": ["ip"],
+            "regex": "N/A"
+        }
+    ]
+}
---
+++ threat_apis.py
@@ -0,0 +1,57 @@
+import requests
+import json
+import re
+import argparse
+from bin import config
+
+parser = argparse.ArgumentParser(
+    description='This tool aggregates multiple different threat feeds and allows searching for various IoCs within these datasets.'
+)
+
+# Required arguments
+required = parser.add_argument_group('required arguments')
+required.add_argument("-i", "--ioc", help="query a indicator.", action="store", type=str, required=True)
+required.add_argument("-t", "--type", help="indicator type (ip, domain, url)", action="store", type=str, required=False)
+required.add_argument("-s", "--source", help=f"name of a given source/feed", action="store", type=str, required=False)
+
+args = parser.parse_args()
+
+build = []
+
+def get_api_details():
+    s = requests.Session()
+    for index, url in enumerate(config.feeds['sources']):
+        r = s.get(url['url'])
+        data = r.text
+        for line in data.split('\n'):
+            line_exclude_filter = re.findall("^\#", line)
+            if not line_exclude_filter:
+                if config.feeds["sources"][index]['regex'] == "N/A":
+                    build.append({"ioc": line.strip('\r'), "type": config.feeds["sources"][index]["type"][0], "source": config.feeds["sources"][index]["name"]})
+                else:
+                    regex_parser = re.findall(config.feeds["sources"][index]['regex'], line)
+                    for line in regex_parser:
+                        build.append({"ioc": line.strip('\r'), "type": config.feeds["sources"][index]["type"][0], "source": config.feeds["sources"][index]["name"]})
+
+def main():
+    get_api_details()
+    for item in build:
+        if (args.ioc).strip() in item["ioc"]:
+            if args.type:
+                if (args.type).strip() in item["type"]:
+                    if args.source:
+                        if (args.source).strip() in item["source"]:
+                           print(item)
+                    else:
+                        print(item)       
+            else:
+                if args.source:
+                    if (args.source).strip() in item["source"]:
+                           print(item)
+                    else:
+                        print(item)
+           
+# Call function
+main()
+
+
